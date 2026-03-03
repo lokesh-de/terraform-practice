aac1c300d4bd340ba66a92eb3e1d93dd74acf57f
Avula Lokesh <lokeshavula80@gmail.com>
2026-03-03 09:00:43 +0530

    multi region setup

--- multi-region/multi-region.tf
+++ multi-region/multi-region.tf
@@ -17,9 +17,9 @@ resource "aws_instance" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu2" {
-    ami = "ami-0b6c6ebed2801a5cb"
+    ami = "ami-0786adace1541ca80"
     instance_type = "t3.micro"
-    key_name = "mykeypair"
-    subnet_id = "subnet-08164473fa277ebed"
+#   key_name = "mykeypair"
+    subnet_id = "subnet-0b362e46a57ea6754"
     provider = aws.us-west-2
 }
