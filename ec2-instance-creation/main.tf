aa6ac4d14edd8919d59cc4b98ab8e9a600a1318b
Avula Lokesh <lokeshavula80@gmail.com>
2026-03-03 07:50:35 +0530

    ec2 creation using terraform

--- ec2-instance-creation/main.tf
+++ ec2-instance-creation/main.tf
@@ -6,4 +6,5 @@ resource "aws_instance" "example" {
     ami           = "ami-0b6c6ebed2801a5cb"  # Specify an appropriate AMI ID
     subnet_id = "subnet-08164473fa277ebed"
     instance_type = "t3.micro"
+    key_name = "mykeypair"
 }
