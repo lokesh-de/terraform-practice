abed548565c65db0fe48f8fab444f2ae85891ddf
Avula Lokesh <lokeshavula80@gmail.com>
2026-03-04 08:43:32 +0530

    Terraform Modules

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -7,4 +7,6 @@ module "ec2_instance" {
   ami_value = "ami-053b0d53c279acc90"
   instance_type_value = "t3.micro"
   subnet_id_value = "subnet-019ea91ed9b5252e7"
+  key_name_value = "mykeypair"
+
 }
--- terraform-modules/modules/ec2-instance/main.tf
+++ terraform-modules/modules/ec2-instance/main.tf
@@ -7,4 +7,5 @@ resource "aws_instance" "example" {
     instance_type = var.instance_type_value
     subnet_id = var.subnet_id_value
     associate_public_ip_address = true
+    key_name = var.key_name_value
 }
